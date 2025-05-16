6f8c1cb9ba3f8513a46b263593dd6e754611f538
manntalati <mann.talati@gmail.com>
2025-05-16 18:15:16 -0500

    frontend

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import yfinance as yf
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/analyze', methods=['GET'])
+def analyze():
+    load_dotenv()
+
+    stock_symbols = [
+        'AAPL',
+        'AMD',
+        'ARM',
+        'AVD',
+        'DELL',
+        'ELF',
+        'NVDA',
+        'PDSB',
+        'PUBM',
+        'QQQ',
+        'TGT',
+        'VRT'
+    ]
+
+    stock_names = []
+
+    for symbol in stock_symbols:
+        dat = yf.Ticker(symbol)
+        try:
+            stock_names.append(dat.info['displayName'])
+        except:
+            try: 
+                stock_names.append(dat.info['shortName'])
+            except:
+                print('No Name Exists')
+            print('No Display Name')
+
+    stock_news = []
+    news_api = os.getenv('NEWS_API')
+    for name in stock_names:
+        url = ('https://newsapi.org/v2/everything?'
+            f'q={name}&'
+            'from=2025-05-12&'
+            'to=2025-05-16&'
+            'sortBy=publishedAt&'
+            'pageSize=2&'
+            f'apiKey={news_api}')
+
+        response = requests.get(url)
+        json = response.json()
+
+        api_token = os.getenv('API_TOKEN')
+        headers = {"Authorization": f"Bearer {api_token}"}
+
+        sentiment_response = requests.post(
+        "https://api-inference.huggingface.co/models/ProsusAI/finbert",
+        headers=headers,
+        json={"inputs": json['articles'][0].get('content')}
+        )
+
+        info = {
+            'Company Name': name,
+            'Article Title': json['articles'][0].get('title'),
+            'Date Published': json['articles'][0].get('publishedAt'),
+            'Article URL': json['articles'][0].get('url'),
+            'Sentiment': sentiment_response.json()[0][0]['label']
+        }
+
+        stock_news.append(info)
+
+    return jsonify(stock_news)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081, debug=True)
---
+++ stock-app/app/(tabs)/explore.tsx
@@ -0,0 +1,80 @@
+import { StyleSheet, Button, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import React, { useState } from 'react';
+
+export default function TabTwoScreen() {
+  const [result, setResult] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const news = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch('http://192.168.86.30:8081/analyze');
+      const data = await res.json();
+      setResult(data);
+    } catch (err) {
+      console.error(err);
+    }
+    setLoading(false);
+  }
+
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={
+        <IconSymbol
+          size={310}
+          color="#808080"
+          name="chevron.left.forwardslash.chevron.right"
+          style={styles.headerImage}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">News On Current Stocks</ThemedText>
+      </ThemedView>
+
+      <Button
+        title={loading ? 'Fetching...' : 'Fetch Current News'}
+        onPress={news}
+        disabled={loading}
+      />
+
+      {result.map((item: any, i: number) => (
+        <View key={i}>
+          <Text style={styles.news}>{item['Company Name']}</Text>
+          <Text style={styles.news}>Article Title: {item['Article Title']}</Text>
+          <Text style={styles.news}>Date Published: {item['Date Published']}</Text>
+          <Text style={styles.news}>Article URL: {item['Article URL']}</Text>
+          <Text style={styles.news}>Sentiment: {item.Sentiment}</Text>
+        </View>
+      ))}
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  news: {
+    fontSize: 20,
+    color: 'white', 
+  },
+  result: {
+    marginTop: 20,
+  },
+  error: {
+    color: 'red',
+  },
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
